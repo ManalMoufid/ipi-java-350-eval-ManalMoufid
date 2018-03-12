6aaceeead09f37cc5291b4fab1fd88e13eeb34c4
Manal <33423507+ManalMoufid@users.noreply.github.com>
2018-03-12 14:46:24 +0100

    Ajout test integré et paramétré

--- src/test/java/com.ipiecoles.java.java350/model/CommercialTest.java
+++ src/test/java/com.ipiecoles.java.java350/model/CommercialTest.java
@@ -1,9 +1,32 @@
 package com.ipiecoles.java.java350.model;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
+        import com.ipiecoles.java.java350.model.Commercial;
+        import junitparams.JUnitParamsRunner;
+        import junitparams.Parameters;
+        import org.assertj.core.api.Assertions;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
 
 public class CommercialTest {
+
+    @Test
+    @Parameters({
+            //"null,500d",
+            "0d, 500d",
+            "10000d,5000d",
+    })
+    public void testPrimeAnnuelle(Double caAnnuel, Double expectedPrime)
+    {
+        //Given : Initialisation des données d'entrée
+        Commercial commercial=new Commercial();
+        commercial.setCaAnnuel(caAnnuel);
+        //When :
+        double prime= commercial.getPrimeAnnuelle();
+        //Then :
+        Assertions.assertThat(prime).isEqualTo(expectedPrime);
+    }
+
 
     @Test
     public void testPrimeAnnuelleWithCANull()
@@ -15,7 +38,35 @@ public class CommercialTest {
         double prime= commercial.getPrimeAnnuelle();
         //Then :
         Assertions.assertThat(prime).isEqualTo(500d);
+    }
+
+    @Test
+    public void testPrimeAnnuelleWithCA0()
+    {
+        //Given
+        Commercial commercial=new Commercial();
+        commercial.setCaAnnuel(0d);
+        //When
+        double prime= commercial.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertThat(prime).isEqualTo(500d);
 
     }
+
+    @Test
+    public void testPrimeAnnuelleWithCA10000()
+    {
+        //Given
+        Commercial commercial=new Commercial();
+        commercial.setCaAnnuel(100000d);
+        //When
+        double prime= commercial.getPrimeAnnuelle();
+        //Then
+        Assertions.assertThat(prime).isGreaterThanOrEqualTo(5000d);
+
+    }
+
+
 
 }
---
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -0,0 +1,89 @@
+package com.ipiecoles.java.java350.repository;
+
+import com.ipiecoles.java.java350.model.Commercial;
+import com.ipiecoles.java.java350.model.Employe;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class EmployeRepositoryTest {
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    Commercial pierreDurand, rachidDurand, manuelPierre;
+
+    @Before
+    public void setUp()
+    {
+        //Supprimer toutes les données de la table employé
+        employeRepository.deleteAll();
+
+        // Ajouter nos données de Test
+        pierreDurand = new Commercial();
+        pierreDurand.setPrenom("Pierre");
+        pierreDurand.setNom("Durand");
+        pierreDurand=employeRepository.save(pierreDurand);
+
+        rachidDurand = new Commercial();
+        rachidDurand.setPrenom("Rachid");
+        rachidDurand.setNom("Durand");
+        rachidDurand=employeRepository.save(rachidDurand);
+
+        manuelPierre = new Commercial();
+        manuelPierre.setPrenom("Manuel");
+        manuelPierre.setNom("Pierre");
+        manuelPierre=employeRepository.save(manuelPierre);
+    }
+
+    @Test
+    public void testFindByNomOuPrenomAllIgnoreCase()
+    {
+        //Given
+
+        //When
+        List<Employe> employeList= employeRepository.findByNomOrPrenomAllIgnoreCase("pierre");
+        //Then
+        Assertions.assertThat(employeList).isNotEmpty();
+        Assertions.assertThat(employeList).hasSize(2);
+        Assertions.assertThat(employeList).contains(pierreDurand);
+        Assertions.assertThat(employeList).contains(manuelPierre);
+
+    }
+
+    @Test
+    public void testFindByNomOuPrenomAllIgnoreCasePrenom()
+    {
+        //Given
+
+        //When
+        List<Employe> employeList= employeRepository.findByNomOrPrenomAllIgnoreCase("RACHID");
+        //Then
+        Assertions.assertThat(employeList).isNotEmpty();
+        Assertions.assertThat(employeList).hasSize(1);
+        Assertions.assertThat(employeList).contains(rachidDurand);
+
+    }
+
+    @Test
+    public void testFindByNomOuPrenomAllIgnoreCaseNotFound()
+    {
+        //Given
+
+        //When
+        List<Employe> employeList= employeRepository.findByNomOrPrenomAllIgnoreCase("Valls");
+        //Then
+        Assertions.assertThat(employeList).isEmpty();
+     ;
+
+    }
+
+}
